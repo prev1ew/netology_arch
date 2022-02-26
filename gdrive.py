feaccd9723ef12f58e027fff36af83b77d9d28ec
prev1ew <warcied1@gmail.com>
2022-02-26 16:14:35 +0200

    Частично не рабочая (доделать - выгрузку фоток в облако)

--- gdrive.py
+++ gdrive.py
@@ -4,15 +4,7 @@ import pickle
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.audio import MIMEAudio
-from email.mime.base import MIMEBase
 from mimetypes import guess_type as guess_mime_type
-
-
-
-# Request all access (permission to read/send/receive emails, manage the inbox, and more)
 from googleapiclient.http import MediaFileUpload
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -38,40 +30,38 @@ def googledrive_authenticate():
     return build('drive', 'v3', credentials=creds)
 
 
-# Adds the attachment with the given filename to the given message
-def add_attachment(message, filename):
-    content_type, encoding = guess_mime_type(filename)
-    if content_type is None or encoding is not None:
-        content_type = 'application/octet-stream'
-    main_type, sub_type = content_type.split('/', 1)
-    if main_type == 'text':
-        fp = open(filename, 'rb')
-        msg = MIMEText(fp.read().decode(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'image':
-        fp = open(filename, 'rb')
-        msg = MIMEImage(fp.read(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'audio':
-        fp = open(filename, 'rb')
-        msg = MIMEAudio(fp.read(), _subtype=sub_type)
-        fp.close()
-    else:
-        fp = open(filename, 'rb')
-        msg = MIMEBase(main_type, sub_type)
-        msg.set_payload(fp.read())
-        fp.close()
-    filename = os.path.basename(filename)
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
-    message.attach(msg)
-
-
-def upload_pic(drive_service, files):
+def upload_pic(drive_service, files, parent_id):
     for file in files:
-        file_metadata = {'name': str(file)}
+        file_metadata = {'name': str(file),
+                         'parents': [parent_id]}
         media = MediaFileUpload(file,
                                 mimetype=guess_mime_type(file)[0])
         file = drive_service.files().create(body=file_metadata,
                                             media_body=media,
                                             fields='id').execute()
 
+
+def create_folder(service, name):
+    file_metadata = {
+        'name': name,
+        'mimeType': 'application/vnd.google-apps.folder',
+        'parents': ''
+    }
+    folder_instagram = service.files().create(body=file_metadata,
+                                                   fields='id').execute()
+    return folder_instagram['id']
+
+
+def get_id_of_folder(service, name_of_folder):
+    page_token = None
+    while True:
+        response = service.files().list(q=f"mimeType='application/vnd.google-apps.folder' and name = '{name_of_folder}'",
+                                              spaces='drive',
+                                              fields='nextPageToken, files(id, name)',
+                                              pageToken=page_token).execute()
+        for file in response.get('files', []):
+            if file['name'] == name_of_folder:
+                return file['id']
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            return create_folder(service, name_of_folder)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import instaloader
 import urllib.request
 import gdrive
 import os
-
+from tqdm import tqdm
+import instaloader
 
 # filename = 'test.jpg'
 # ig = instaloader.Instaloader()
@@ -25,44 +25,71 @@ class Arch:
         self.filename = "test.jpg"
         self.dp = "iwanshes"
         self.ig = instaloader.Instaloader()
-        # self.is_filled = False
+        self.is_filled = False
+        self.folder_instagram = ''
 
-    def download(self):
+    def download_profile_pic(self):
         profile = self.ig.check_profile_id(self.dp.lower())
         url_pic = profile.profile_pic_url
         urllib.request.urlretrieve(url_pic, self.filename)
 
+    def download_with_progressbar(self):
+        todo_list = [self.download_profile_pic]
+        for item in tqdm(todo_list):
+            item()
+
     def send(self):
         service = gdrive.googledrive_authenticate()
+        parent_id = gdrive.get_id_of_folder(service, arch.dp)
         files = [self.filename]
-        gdrive.upload_pic(service, files)
+        gdrive.upload_pic(service, files, parent_id)
 
     def delete(self):
         if os.path.isfile(self.filename):
             os.remove(self.filename)
 
     def archive(self):
-        self.download()
-        self.send()
-        self.delete()
+        todo_list = [self.download_profile_pic, self.send, self.delete]
+        for item in tqdm(todo_list):
+            item()
 
-        print('arch')
-        pass
+    def archive_latest_posts(self):
+        inputs(self)
+        prof = self.ig.check_profile_id(self.dp.lower())
+        max_count = 5
+        count = 1
+        for post in prof.get_posts():
+            res = self.ig.download_post(post, self.dp)
+            photo_name = str(post.date) + '.jpg'
+            urllib.request.urlretrieve(post.url, photo_name)
+            print(res)
+
+            if max_count < count:
+                break
+
+            count += 1
 
 
 def inputs(arche):
-    # if not arche.is_filled:
-    newname = input("Filename: ") + '.jpg'
-    if newname != '.jpg':
-        arche.filename = newname
+    if not arche.is_filled:
+        newname = input("Filename: ") + '.jpg'
+        if newname != '.jpg':
+            arche.filename = newname
 
-    arche.dp = input("Username: ")
+        arche.dp = input("Username: ")
+
+
+
 
 
 arch = Arch()
 
 while True:
-    n = input("Write '1' for archive to GDrive '2' for download file or something else to Exit\n")
+    n = input("1 = Archive avatar \n "
+              "2 = Download avatar \n "
+              "3 = Archive latest posts \n "
+              "Any other input = exit \n "
+              "Your input: ")
     if n == '1':
         inputs(arch)
         arch.archive()
@@ -70,8 +97,9 @@ while True:
 
     elif n == '2':
         inputs(arch)
-        arch.download()
-        print("File is Download")
-
+        arch.download_with_progressbar()
+        print("File is Downloaded")
+    elif n == '3':
+        arch.archive_latest_posts()
     else:
         break
